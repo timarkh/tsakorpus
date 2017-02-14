08de785883fc5971816e64d2f8b06db1ca099ebe
maryszmary <masha.shejanova@gmail.com>
2017-02-14 23:08:36 +0300

    a draft of corpus-generating script

---
+++ generate_rand_lang.py
@@ -0,0 +1,26 @@
+
+'''
+a script for generating a random language with changable characteristics
+'''
+
+import string
+
+ALPHABET = 'qwertyuiopasdfghjklzxcvbnm'
+PUNKT = string.punctuation
+MEAN_WORD_LENGTH = 5
+MEAN_SENT_LENGTH = 10
+LENGTH_OF_CORPUS = 15*10^6 # in tokens?
+POS = {}
+
+
+def generate_words():
+	pass
+
+
+def generate_sents():
+	pass
+
+
+def generate_texts():
+	pass
+
