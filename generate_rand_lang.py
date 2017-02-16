a225568616549b0ecf5e8aeb62bb8c8d606f7b62
maryszmary <masha.shejanova@gmail.com>
2017-02-16 15:45:00 +0300

    assigning frequencies

--- generate_rand_lang.py
+++ generate_rand_lang.py
@@ -41,7 +41,7 @@ PUNKT = string.punctuation
 MEAN_WORD_LENGTH = 5
 MEAN_SENT_LENGTH = 10
 MAX_SENT_LENGTH = 40
-LENGTH_OF_CORPUS =  15*10**6
+LENGTH_OF_CORPUS =  15*10**5
 N_0 = 20000 # a constant for zipf's low
 # with open('grammar_features.json') as f:
 #     GRAMMAR = json.load(f)
@@ -59,15 +59,40 @@ class Lexeme():
         self.pos = pos
         
 
-def generate_words():
+def generate_wordforms():
+    '''returns a list of wordforms (instances) with freqs'''
+    n = calc_num_of_words()
+    wfms_with_freq = []
+    first = calc_f_0(n)
+    for i in range(n//2):
+        freq = first//(i + 1)
+        print('current_freq: ' + str(freq))
+        wfms_with_freq.append(WordForm(freq))
+    wfms_with_freq += [WordForm(1) for i in range(n - n//2)]
+    print(sum([wf.freq for wf in wfms_with_freq]))
+    return wfms_with_freq
+
+
+def make_freqs():
+    n = calc_num_of_words()
+    f_0 = culc_f_0(n)
     pass
+
+
+def calc_f_0(n):
+    sum_of_row = 0
+    i = n//2
+    while i >= 1:
+        sum_of_row += 1/i
+        i -= 1
+    return round((LENGTH_OF_CORPUS - n/2)/sum_of_row)
 
 
 def calc_num_of_words():
     gamma = 0.577
     disc = 1/4 + 2 * LENGTH_OF_CORPUS * ((math.log(N_0) + gamma + 1)/ N_0)
-    N = 2 * (1/2 + math.sqrt(disc)) * N_0/(math.log(N_0) + gamma + 1)
-    return N
+    n = 2 * (1/2 + math.sqrt(disc)) * N_0/(math.log(N_0) + gamma + 1)
+    return round(n)
 
 def generate_sents(words_and_prob):
     pass
@@ -86,4 +111,4 @@ def generate_corp():
 # if __name__ == '__main__':
 #     generate_corp()
     
-print(calc_num_of_words())
+generate_wordforms()
