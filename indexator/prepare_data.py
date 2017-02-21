fe4437b9be636f9082c00a0402ade2c6c4e36325
Timofey Arkhangelskiy <timarkh@gmail.com>
2017-02-21 12:54:23 +0300

    Added Elasticsearch mapping generator.

---
+++ indexator/prepare_data.py
@@ -0,0 +1,64 @@
+import json
+import os
+import re
+
+
+class PrepareData:
+    """
+    Contains functions called when preparing the data
+    for indexing in the database.
+    """
+    SETTINGS_DIR = '../conf'
+    rxBadField = re.compile('[^a-z0-9_]|^(?:lex|gr)$')
+
+    def __init__(self):
+        f = open(os.path.join(self.SETTINGS_DIR, 'word_fields.json'),
+                 'r', encoding='utf-8')
+        self.wordFields = json.loads(f.read())
+        f.close()
+        f = open(os.path.join(self.SETTINGS_DIR, 'categories.json'),
+                 'r', encoding='utf-8')
+        self.categories = json.loads(f.read())
+        f.close()
+
+    def generate_words_mapping(self):
+        """
+        Return Elasticsearch mapping for the type "word", based
+        on searchable features described in word_fields.json and
+        categories.json.
+        """
+        m = {'wf': {'type': 'text'},
+             'wtype': {'type': 'keyword'},
+             'sent_ids': {'type': 'integer', 'index': False},
+             'ana.lex': {'type': 'text'}}
+        for field in self.wordFields:
+            if self.rxBadField.search(field) is None:
+                m['ana.' + field] = {'type': 'text'}
+        for field in set(self.categories.values()):
+            if self.rxBadField.search(field) is None:
+                m['ana.gr.' + field] = {'type': 'keyword'}
+        return m
+
+    def generate_mappings(self):
+        """
+        Return Elasticsearch mappings for all types to be used
+        in the corpus database.
+        """
+        mWord = {'properties': self.generate_words_mapping()}
+        mappings = {'mappings': {'word': mWord}}
+        return mappings
+
+    def write_mappings(self, fnameOut):
+        """
+        Generate and write Elasticsearch mappings for all types to be used
+        in the corpus database.
+        """
+        fOut = open(fnameOut, 'w', encoding='utf-8')
+        fOut.write(json.dumps(self.generate_mappings(), indent=2,
+                              ensure_ascii=False))
+        fOut.close()
+
+
+if __name__ == '__main__':
+    pd = PrepareData()
+    pd.write_mappings('mappings.json')
