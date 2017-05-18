68f0af9dea0d182963b1fce25cd359b75f20888c
Timofey Arkhangelskiy <timarkh@gmail.com>
2017-05-18 11:46:05 +0200

    Added some css and word table sorting.

--- search/web_app/static/js/result_parser.js
+++ search/web_app/static/js/result_parser.js
@@ -88,3 +88,26 @@ function assign_gram_popup() {
 		$('div#analysis').css('top', e.pageY + moveDown).css('left', e.pageX + moveLeft);
 	});
 }
+
+function make_sortable() {
+	$('th').click(function(){
+		var table = $(this).parents('table').eq(0);
+		var rows = table.find('tr:gt(0)').toArray().sort(row_comparer($(this).index()));
+		this.asc = !this.asc;
+		if (!this.asc) {
+			rows = rows.reverse()
+		}
+		for (var i = 0; i < rows.length; i++) {
+			table.append(rows[i])
+		}
+	})
+}
+
+function row_comparer(index) {
+    return function(a, b) {
+        var valA = getCellValue(a, index), valB = getCellValue(b, index);
+        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB);
+    }
+}
+
+function getCellValue(row, index){ return $(row).children('td').eq(index).html(); }
