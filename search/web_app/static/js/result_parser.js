79c6b3e61665d174465fd65e27f545fd947069fb
Timofey Arkhangelskiy <timarkh@gmail.com>
2018-01-26 18:34:30 +0100

    Fixed a bug in the web interface and added some metafield translations.

--- search/web_app/static/js/result_parser.js
+++ search/web_app/static/js/result_parser.js
@@ -283,16 +283,18 @@ function assign_gram_popup() {
 		$('#analysis').css('left', $(document).innerWidth() - anaWidth - 30);
 		$('#analysis').css('top', $(document).innerHeight() - anaHeight - 30);
 		$('#analysis').show();
-        var prevEl = $(this).prev();
-        while (true) {
-            if (prevEl.hasClass('sentence_meta')) {
-                break;
-            }
-            prevEl = prevEl.prev();
-        }
-        if (prevEl.hasClass('sentence_meta')) {
-            $('.sentence_meta').hide();
-            prevEl.show();
+        if ($('.sentence_meta').length > 0) {
+			var prevEl = $(this).prev();
+			while (true) {
+				if (prevEl.hasClass('sentence_meta')) {
+					break;
+				}
+				prevEl = prevEl.prev();
+			}
+			if (prevEl.hasClass('sentence_meta')) {
+				$('.sentence_meta').hide();
+				prevEl.show();
+			}
         }
 	}, function () {
 		$('#analysis').hide();
