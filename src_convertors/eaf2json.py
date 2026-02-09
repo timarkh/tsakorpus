af49bfdc42d1424c5b9f5a815a572258e36a3c80
Timofey Arkhangelskiy <timarkh@gmail.com>
2026-02-09 15:10:57 +0100

    Bug fix

--- src_convertors/eaf2json.py
+++ src_convertors/eaf2json.py
@@ -339,8 +339,8 @@ class Eaf2JSON(Txt2JSON):
                                 elif type(totalAna[k]) == list and ana[k] not in totalAna[k]:
                                     totalAna[k].append(ana[k])
                         else:
-                            if (len(totalAna[k]) > 0 and (k != 'parts' or ('hyphens_in_gloss' in self.corpusSettings
-                                    and self.corpusSettings['hyphens_in_gloss'] and k != 'gloss'))):
+                            if (len(totalAna[k]) > 0 and (k != 'parts' and not ('hyphens_in_gloss' in self.corpusSettings
+                                    and self.corpusSettings['hyphens_in_gloss'] and k == 'gloss'))):
                                 totalAna[k] += '-'
                             if k not in ana:
                                 totalAna[k] += '∅'
